ef8b1f69c3d87e9c7a4263f6000514d42a7daa0d
Fernando_OM <fernando.ordaz@tekoestudio.com>
2022-09-05 20:10:26 -0500

    Fix: update namespace in SwaggerConfig

--- backend/src/main/java/com/fernando/zssn/context/SwaggerConfig.java
+++ backend/src/main/java/com/fernando/zssn/context/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.fernando.zssn.v1.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.fernando.zssn.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
