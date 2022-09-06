06e8129a0b9cd2f3ef394f329bed7ecaaccfb111
Fernando_OM <fernando.ordaz@tekoestudio.com>
2022-09-05 19:36:54 -0500

    Feat: add controller to update a survivor location

--- backend/src/main/java/com/fernando/zssn/controller/SurvivorsController.java
+++ backend/src/main/java/com/fernando/zssn/controller/SurvivorsController.java
@@ -2,6 +2,7 @@ package com.fernando.zssn.controller;
 
 import com.fernando.zssn.presentation.JsonFormatHandler;
 import com.fernando.zssn.service.SurvivorService;
+import com.fernando.zssn.service.dto.LocationRequestDto;
 import com.fernando.zssn.service.dto.SurvivorRequestDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,11 +20,10 @@ public class SurvivorsController {
     @PostMapping
     public ResponseEntity<JsonFormatHandler> createSurvivor(@RequestBody SurvivorRequestDto survivorRequest) {
         return this.survivorService.createSurvivor(survivorRequest).getResponse();
-        //return new ResponseEntity<>(survivor, HttpStatus.CREATED);
     }
 
-    @GetMapping
-    public String hello() {
-        return "Hola";
+    @PutMapping(value = "/{id}/location")
+    public ResponseEntity<JsonFormatHandler> updateSurvivorLocation(@PathVariable Long id, @RequestBody LocationRequestDto locationRequest) {
+        return this.survivorService.updateSurvivorLocation(id, locationRequest).getResponse();
     }
 }
