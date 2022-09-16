6f3f4213d760d7387ffa7ca85ea4cb6166ad07bf
fernando_OM <thbig4@gmail.com>
2022-09-16 15:10:04 -0500

    Feat: fetch all items

--- backend/src/main/java/com/fernando/zssn/persistence/entity/Item.java
+++ backend/src/main/java/com/fernando/zssn/persistence/entity/Item.java
@@ -16,7 +16,7 @@ import javax.persistence.*;
 public class Item {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Enumerated(EnumType.STRING)
     private ItemType type;
